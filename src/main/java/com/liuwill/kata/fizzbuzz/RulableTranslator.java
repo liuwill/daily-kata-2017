07e2a1835d67b5c42d592bb9057a9e40ecff4559
liuwill <liuwei_will@qq.com>
2017-07-20 21:13:48 +0800

    FINISH: fizz buzz kata

--- src/main/java/com/liuwill/kata/fizzbuzz/RulableTranslator.java
+++ src/main/java/com/liuwill/kata/fizzbuzz/RulableTranslator.java
@@ -6,9 +6,6 @@ import java.util.List;
  * Created by Liu Will on 2017/7/19.
  */
 public class RulableTranslator {
-    public static final String FIZZ_WORD = "Fizz";
-    public static final String BUZZ_WORD = "Buzz";
-
     public static String translate(List<TranslateRule> rules, Integer i) {
         StringBuilder builder = new StringBuilder();
         for (TranslateRule rule : rules) {
